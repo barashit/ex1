739edb631a0afa95efefb5c9e7126b7923d0134c
Pesna <prostopesna74@gmail.com>
2023-06-21 21:10:17 +0400

    Initial commit: add maths function

---
+++ main.cpp
@@ -0,0 +1,14 @@
+namespace varya
+{
+    int add(int a, int b)
+    {
+        int result = a + b;
+        return result;
+    }
+
+       int sub(int c, int d)
+    {
+        int result = c - d;
+        return result;
+    }
+} 
